c6c5953d7a6bb103be750519829f2a6709e9bc14
Ivan Sebastian <ivantedja@gmail.com>
2017-01-06 19:37:57 +0700

    some UI improvement

--- src/App.js
+++ src/App.js
@@ -20,12 +20,6 @@ class Congklak {
         $('#log').append(msg);
     }
 
-    // taken from: http://www.sean.co.uk/a/webdesign/javascriptdelay.shtm
-    pause(ms = this.delayMs) {
-        ms += new Date().getTime();
-        while (new Date() < ms){}
-    }
-
     printBoard() {
         const leftPadding = (str) => {
             return ('    ' + str).slice(-4);
@@ -64,7 +58,11 @@ class Congklak {
         // first row (north player)
         html += '<tr><td></td>';
         for (let i = this.board.numOfHolesAndStones + 1; i <= 2 * this.board.numOfHolesAndStones; i++) {
-            html += '<td><span class="hole" data-hole="' + i + '">' + this.board.holes[i].stones.length + '</span></td>';
+            let cssClass = ['hole'];
+            if (this.board.holes[i].isBlocked) {
+                cssClass.push('blocked');
+            }
+            html += '<td><span class="' + cssClass.join(' ') + '" data-hole="' + i + '">' + this.board.holes[i].stones.length + '</span></td>';
         }
         html += '<td></td></tr>';
 
@@ -80,7 +78,11 @@ class Congklak {
         // third row (south player)
         html += '<tr><td></td>';
         for (let i = this.board.numOfHolesAndStones - 1; i >= 0; i--) {
-            html += '<td><span class="hole" data-hole="' + i + '">' + this.board.holes[i].stones.length + '</span></td>';
+            let cssClass = ['hole'];
+            if (this.board.holes[i].isBlocked) {
+                cssClass.push('blocked');
+            }
+            html += '<td><span class="' + cssClass.join(' ') + '" data-hole="' + i + '">' + this.board.holes[i].stones.length + '</span></td>';
         }
         html += '<td></td></tr>';
 
@@ -89,7 +91,7 @@ class Congklak {
         return html;
     }
 
-    observeOnClickEvent() {
+    observeOnClickHole() {
         const self = this;
         $('.hole').each((idx, elem) => {
             $(elem).click(() => {
@@ -100,26 +102,16 @@ class Congklak {
 
     drawBoardUI() {
         $('#congklak').html(this.getBoardHtml());
-        this.observeOnClickEvent();
+        this.observeOnClickHole();
     }
 
-    drawFromQueue() {
-        let counter = 0;
-        console.log(this.queueDisplay);
-        const start = new Date().getTime();
-        console.log('start', start);
-        const queueLength = this.queueDisplay.length;
-        for (let i = 1; i <= queueLength; i++) {
-            setTimeout(function() {
-                let now = new Date().getTime();
-                console.log(now - start, 'comp', i * 1000);
-                if (now - start > i * 1000) {
-                    let html = this.queueDisplay.shift();
-                    console.log(html);
-                    $('#congklak').html(html);
-                }
-            }.bind(this), 100);
-        }
+    showRoundControls() {
+        $('#round-controls').show();
+        $('#round-controls').find('button').each((idx, elem) => {
+            $(elem).click(() => {
+                $('#round-controls').hide();
+            });
+        });
     }
 
     //============== main logic ==============//
@@ -160,11 +152,44 @@ class Congklak {
     }
 
     endRound(winner) {
+        this.showRoundControls();
+        // debug
+        this.printBoard();
+        this.drawBoardUI();
         this.showMessage('Round is over! Round winner: ' + winner);
     }
 
     endGame(winner) {
+        // debug
+        this.printBoard();
+        this.drawBoardUI();
         this.showMessage('Game is over! Game winner: ' + winner);
+    }
+
+    forceEndGame() {
+        this.board.cleanupStones();
+        const playerSouthStoreHouse = this.board.getPlayerSouthStoreHouse();
+        const playerNorthStoreHouse = this.board.getPlayerNorthStoreHouse();
+        const playerSouthStones = playerSouthStoreHouse.stones.length;
+        const playerNorthStones = playerNorthStoreHouse.stones.length;
+        let winner = 'draw';
+        if (playerSouthStones > playerNorthStones) {
+            winner = playerSouthStoreHouse.player.id;
+        } else if (playerNorthStones > playerSouthStones) {
+            winner = playerNorthStoreHouse.player.id;
+        }
+        this.showMessage('Force end game. Winner: ' + winner);
+        // debug
+        this.printBoard();
+        this.drawBoardUI();
+    }
+
+    nextRound() {
+        this.showMessage('Continue next round.');
+        this.board.initNextRound();
+        // debug
+        this.printBoard();
+        this.drawBoardUI();
     }
 
     chooseHoleAndDistribute(holeIndex, isHumanChoice = true) {
@@ -192,6 +217,8 @@ class Congklak {
         }
 
         let stonesInHand = startHole.takeAllStones();
+        // debug
+        this.printBoard();
 
         // distribute all stones in hand until empty
         while (stonesInHand.length > 0) {
@@ -201,12 +228,9 @@ class Congklak {
             nextHole.addStone(stonesInHand.pop());
             // mark as moved to next hole
             holeIndex = nextHoleIndex;
-
+            // debug
             this.printBoard();
         }
-
-        // draw UI
-        this.drawBoardUI();
 
         // record last hole
         const lastHoleIndex = holeIndex;
@@ -223,16 +247,13 @@ class Congklak {
         //       because it contains our last stone
 
         if (lastHole.isStoreHouse && lastHole.player.id === currentPlayer.id) {
-            this.showMessage('You\'re landed in your store house. Continue.');
+            this.showMessage('You\'re landed in your store house. Run again.');
             // can choose another hole, currentPlayer stay the same
             // end game
             if (this.checkGameOverAndEnd()) {
                 return;
             }
         } else if (lastHole.stones.length > 1) {
-            // debug
-            this.printBoard();
-            this.drawBoardUI();
             // non empty holes, repeat
             // 2nd parameter indicating that it is not a human choice
             this.chooseHoleAndDistribute(lastHoleIndex, false);
@@ -243,6 +264,7 @@ class Congklak {
             }
             this.board.switchPlayer();
             this.showMessage('Switch player into: ' + this.board.getCurrentPlayer().id);
+            this.drawBoardUI();
         } else if (lastHole.stones.length === 1 && lastHole.player.id === currentPlayer.id) {
             const oppositeHoleIndex = this.board.getOppositeHoleIndex(lastHoleIndex);
             const oppositeHole = this.board.holes[oppositeHoleIndex];
@@ -262,6 +284,7 @@ class Congklak {
                 }
                 this.board.switchPlayer();
                 this.showMessage('Switch player into: ' + this.board.getCurrentPlayer().id);
+                this.drawBoardUI();
             } else {
                 // end game or switch player
                 if (this.checkGameOverAndEnd()) {
@@ -269,6 +292,7 @@ class Congklak {
                 }
                 this.board.switchPlayer();
                 this.showMessage('Switch player into: ' + this.board.getCurrentPlayer().id);
+                this.drawBoardUI();
             }
         }
     }
