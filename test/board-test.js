0dfdd29c5f65771ee72af4512801794400143dca
Ivan Sebastian <ivantedja@gmail.com>
2017-01-07 12:12:34 +0700

    better unit testing for board

--- test/board-test.js
+++ test/board-test.js
@@ -4,76 +4,60 @@ import Board from '../src/model/Board';
 import Stone from '../src/model/Stone';
 
 describe('test board', function() {
-    var board = new Board();
+    var board;
+
+    beforeEach(function () {
+        board = new Board();
+    });
 
     describe('test initial stones in each hole', function() {
-        var expectedStones = [7, 7, 7, 7, 7, 7, 7, 0, 7, 7, 7, 7, 7, 7, 7, 0];
-        var i = 0;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 0', function() {
+            assert.equal(board.holes[0].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 1', function() {
+            assert.equal(board.holes[1].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 2', function() {
+            assert.equal(board.holes[2].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 3', function() {
+            assert.equal(board.holes[3].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 4', function() {
+            assert.equal(board.holes[4].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 5', function() {
+            assert.equal(board.holes[5].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 6', function() {
+            assert.equal(board.holes[6].stones.length, 7);
         });
-
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 7', function() {
+            assert.equal(board.holes[7].stones.length, 0);
         });
-
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 8', function() {
+            assert.equal(board.holes[8].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 9', function() {
+            assert.equal(board.holes[9].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 10', function() {
+            assert.equal(board.holes[10].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 11', function() {
+            assert.equal(board.holes[11].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 12', function() {
+            assert.equal(board.holes[12].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 13', function() {
+            assert.equal(board.holes[13].stones.length, 7);
         });
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 14', function() {
+            assert.equal(board.holes[14].stones.length, 7);
         });
-
-        i++;
-        it('there should be ' + expectedStones[i] + ' stones in hole ' + i, function() {
-            assert.equal(board.holes[i].stones.length, expectedStones[i]);
+        it('there should be 7 stones in hole index 15', function() {
+            assert.equal(board.holes[15].stones.length, 0);
         });
     });
 
@@ -84,87 +68,84 @@ describe('test board', function() {
         it('current player should be player south', function() {
             assert.equal(board.getCurrentPlayer().id, 'south');
         });
-        it('swith player should become player north', function() {
+        it('switch player should become player north', function() {
             board.switchPlayer();
             assert.equal(board.getCurrentPlayer().id, 'north');
-            board.switchPlayer();
         });
-        it('double swith player should become player south again', function() {
+        it('double switch player should become player south', function() {
             board.switchPlayer();
             board.switchPlayer();
             assert.equal(board.getCurrentPlayer().id, 'south');
         });
         describe('test empty holes', function() {
             describe('player south hole empty', function() {
-                var boardTmp = new Board();
-                var mockStones = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-                for (var i = 0; i < mockStones.length; i++) {
-                    var stonesTmp = [];
-                    for (var j = 0; j < mockStones[i]; j++) {
-                        stonesTmp.push(new Stone());
+                beforeEach(function() {
+                    var mockStones = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+                    for (var i = 0; i < mockStones.length; i++) {
+                        var stonesTmp = [];
+                        for (var j = 0; j < mockStones[i]; j++) {
+                            stonesTmp.push(new Stone());
+                        }
+                        board.holes[i].stones = stonesTmp;
                     }
-                    boardTmp.holes[i].stones = stonesTmp;
-                }
+                });
                 it('player south hole should be empty', function() {
-                    assert.isOk(boardTmp.isPlayerSouthHolesEmpty());
+                    assert.isOk(board.isPlayerSouthHolesEmpty());
                 });
 
-                it('check any empty should swith to player north', function() {
-                    boardTmp.isAnyPlayerHolesEmptyAndSetNextPlayer();
-                    assert.equal(boardTmp.getCurrentPlayer().id, 'north');
+                it('check any empty should switch to player north', function() {
+                    board.isAnyPlayerHolesEmptyAndSetNextPlayer();
+                    assert.equal(board.getCurrentPlayer().id, 'north');
                 });
             });
             describe('player north hole empty', function() {
-                var boardTmp = new Board();
-                var mockStones = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1];
-                for (var i = 0; i < mockStones.length; i++) {
-                    var stonesTmp = [];
-                    for (var j = 0; j < mockStones[i]; j++) {
-                        stonesTmp.push(new Stone());
+                beforeEach(function() {
+                    var mockStones = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1];
+                    for (var i = 0; i < mockStones.length; i++) {
+                        var stonesTmp = [];
+                        for (var j = 0; j < mockStones[i]; j++) {
+                            stonesTmp.push(new Stone());
+                        }
+                        board.holes[i].stones = stonesTmp;
                     }
-                    boardTmp.holes[i].stones = stonesTmp;
-                }
+                });
                 it('player north hole should be empty', function() {
-                    assert.isOk(boardTmp.isPlayerNorthHolesEmpty());
+                    assert.isOk(board.isPlayerNorthHolesEmpty());
                 });
 
-                it('check any empty should swith to player south', function() {
-                    boardTmp.isAnyPlayerHolesEmptyAndSetNextPlayer();
-                    assert.equal(boardTmp.getCurrentPlayer().id, 'south');
+                it('check any empty should switch to player south', function() {
+                    board.isAnyPlayerHolesEmptyAndSetNextPlayer();
+                    assert.equal(board.getCurrentPlayer().id, 'south');
                 });
             });
             describe('test cleanup', function() {
                 it('no stones present in player south', function() {
-                    var boardTmp = new Board();
-                    boardTmp.cleanupStones();
-                    assert.equal(boardTmp.holes[0].stones.length, 0);
-                    assert.equal(boardTmp.holes[1].stones.length, 0);
-                    assert.equal(boardTmp.holes[2].stones.length, 0);
-                    assert.equal(boardTmp.holes[3].stones.length, 0);
-                    assert.equal(boardTmp.holes[4].stones.length, 0);
-                    assert.equal(boardTmp.holes[5].stones.length, 0);
-                    assert.equal(boardTmp.holes[6].stones.length, 0);
+                    board.cleanupStones();
+                    assert.equal(board.holes[0].stones.length, 0);
+                    assert.equal(board.holes[1].stones.length, 0);
+                    assert.equal(board.holes[2].stones.length, 0);
+                    assert.equal(board.holes[3].stones.length, 0);
+                    assert.equal(board.holes[4].stones.length, 0);
+                    assert.equal(board.holes[5].stones.length, 0);
+                    assert.equal(board.holes[6].stones.length, 0);
                 });
                 it('no stones present in player north', function() {
-                    var boardTmp = new Board();
-                    boardTmp.cleanupStones();
-                    assert.equal(boardTmp.holes[8].stones.length, 0);
-                    assert.equal(boardTmp.holes[9].stones.length, 0);
-                    assert.equal(boardTmp.holes[10].stones.length, 0);
-                    assert.equal(boardTmp.holes[11].stones.length, 0);
-                    assert.equal(boardTmp.holes[12].stones.length, 0);
-                    assert.equal(boardTmp.holes[13].stones.length, 0);
-                    assert.equal(boardTmp.holes[14].stones.length, 0);
+                    board.cleanupStones();
+                    assert.equal(board.holes[8].stones.length, 0);
+                    assert.equal(board.holes[9].stones.length, 0);
+                    assert.equal(board.holes[10].stones.length, 0);
+                    assert.equal(board.holes[11].stones.length, 0);
+                    assert.equal(board.holes[12].stones.length, 0);
+                    assert.equal(board.holes[13].stones.length, 0);
+                    assert.equal(board.holes[14].stones.length, 0);
                 });
                 it('there should be 49 stones in player south store house', function() {
-                    var boardTmp = new Board();
-                    boardTmp.cleanupStones();
-                    assert.equal(boardTmp.getPlayerSouthStoreHouse().stones.length, 49);
+                    board.cleanupStones();
+                    assert.equal(board.getPlayerSouthStoreHouse().stones.length, 49);
                 });
                 it('there should be 49 stones in player south store house', function() {
-                    var boardTmp = new Board();
-                    boardTmp.cleanupStones();
-                    assert.equal(boardTmp.getPlayerNorthStoreHouse().stones.length, 49);
+                    board.cleanupStones();
+                    assert.equal(board.getPlayerNorthStoreHouse().stones.length, 49);
                 });
             });
         });
@@ -279,90 +260,59 @@ describe('test board', function() {
         });
 
         describe('test next hole for player north', function() {
+            beforeEach(function() {
+                board.switchPlayer();
+            });
             it('next hole for hole index 0 should be 1', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(0), 1);
-                board.switchPlayer();
             });
             it('next hole for hole index 1 should be 2', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(1), 2);
-                board.switchPlayer();
             });
             it('next hole for hole index 2 should be 3', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(2), 3);
-                board.switchPlayer();
             });
             it('next hole for hole index 3 should be 4', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(3), 4);
-                board.switchPlayer();
             });
             it('next hole for hole index 4 should be 5', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(4), 5);
-                board.switchPlayer();
             });
             it('next hole for hole index 5 should be 6', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(5), 6);
-                board.switchPlayer();
             });
             it('next hole for hole index 6 should be 7', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(6), 8);
-                board.switchPlayer();
             });
             it('next hole for hole index 7 should be 8', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(7), 8);
-                board.switchPlayer();
             });
             it('next hole for hole index 8 should be 9', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(8), 9);
-                board.switchPlayer();
             });
             it('next hole for hole index 9 should be 10', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(9), 10);
-                board.switchPlayer();
             });
             it('next hole for hole index 10 should be 11', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(10), 11);
-                board.switchPlayer();
             });
             it('next hole for hole index 11 should be 12', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(11), 12);
-                board.switchPlayer();
             });
             it('next hole for hole index 12 should be 13', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(12), 13);
-                board.switchPlayer();
             });
             it('next hole for hole index 13 should be 14', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(13), 14);
-                board.switchPlayer();
             });
             it('next hole for hole index 14 should be 15', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(14), 15);
-                board.switchPlayer();
             });
             it('next hole for hole index 15 should be 0', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(15), 0);
-                board.switchPlayer();
             });
             it('next hole for hole index 16 should be 0? (out of bound though)', function() {
-                board.switchPlayer();
                 assert.equal(board.getNextHoleIndex(16), 0);
-                board.switchPlayer();
             });
         });
 
