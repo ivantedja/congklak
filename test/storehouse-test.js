cf3f123c4d63bdf48fb40d2fca4dcd009bac0c8d
Ivan Sebastian <ivantedja@gmail.com>
2017-01-07 11:21:44 +0700

    better unit testing for store house

--- test/storehouse-test.js
+++ test/storehouse-test.js
@@ -4,23 +4,24 @@ import StoreHouse from '../src/model/StoreHouse';
 import Player from '../src/model/Player';
 
 describe('test store house', function() {
+    var storeHouse;
+
+    beforeEach(function () {
+        var player = new Player('south');
+        storeHouse = new StoreHouse(player);
+    });
+
     describe('test store house is a store house', function() {
         it('it is a store house', function() {
-            var storeHouse = new StoreHouse();
             assert.isOk(storeHouse.isStoreHouse);
         });
     });
 
     describe('test hole belongs to player', function() {
-        var player = new Player('south');
         it('should belongs to player south', function() {
-            var storeHouse = new StoreHouse(player);
-
             assert.equal(storeHouse.player.id, 'south');
         });
         it('should not belongs to player north', function() {
-            var storeHouse = new StoreHouse(player);
-
             assert.notEqual(storeHouse.player.id, 'north');
         });
     });
